0b73d1a6bd10b6e01c01199eb619d229cb85471f
Bonaldo <dnllink@hotmail.com>
2015-10-08 20:33:53 -0300

    criada pagina inicial servida pelo Express

--- config/express.js
+++ config/express.js
@@ -1,7 +1,11 @@
 var express = require('express');
 
 module.exports = express.exports = function () {
+    
     var app = express();
     app.set('port', 3000);
+    app.use(express.static('./public'));
+    
     return app;
+    
 };
