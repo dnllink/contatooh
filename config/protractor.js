1c922b1eba05eb5bd60f3fdff3281ab8d3150aeb
Bonaldo <dnllink@hotmail.com>
2015-11-21 11:44:37 -0200

    removidos dados de acesso

--- config/protractor.js
+++ config/protractor.js
@@ -1,17 +1,10 @@
 exports.config = {
     specs: ['../test/e2e/**/*.js'],
     onPrepare: function () {
-        //        browser.driver.get('http://localhost:3000');
-        //        browser.driver.findElement(by.id('entrar')).click();
-        //        browser.driver.findElement(by.id('login_field')).sendKeys('dnllink@hotmail.com');
-        //        browser.driver.findElement(by.id('password')).sendKeys('824637nu');
-        //        browser.driver.findElement(by.name('commit')).click();
-
-        //código refatorado para utilizar auth.html com AngularJS
         browser.get('http://localhost:3000');
         element(by.id('entrar')).click();
-        browser.driver.findElement(by.id('login_field')).sendKeys('dnllink@hotmail.com');
-        browser.driver.findElement(by.id('password')).sendKeys('824637nu');
+        browser.driver.findElement(by.id('login_field')).sendKeys('');
+        browser.driver.findElement(by.id('password')).sendKeys('');
         browser.driver.findElement(by.name('commit')).click();
     }
 }
