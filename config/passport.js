c6f3282ee0626fa1b916411cfbd64d3c77978aba
Bonaldo <dnllink@hotmail.com>
2015-11-21 21:18:38 -0200

    finalizada a implementacao dos testes e2e com Sauce Labs

--- config/passport.js
+++ config/passport.js
@@ -1,42 +1,45 @@
 var passport = require('passport');
 var GitHubStrategy = require('passport-github').Strategy;
 var mongoose = require('mongoose');
+var config = require('./config')();
 
-module.exports = function(){
-    
+module.exports = function () {
+
     var Usuario = mongoose.model('Usuario');
-    
+
     passport.use(new GitHubStrategy({
-        clientID: '1d7b9c72cb89ff5c4111',
-        clientSecret: '2c2c70d68b8938e56ac56ad8d846e40aba6fb6f3',
+        clientID: config.clientID,
+        clientSecret: config.clientSecret,
         callbackURL: 'http://localhost:3000/auth/github/callback'
     }, function (accessToken, refreshToken, profile, done) {
-        
-        Usuario.findOrCreate(
-            { "login" : profile.username },
-            { "nome" : profile.username },
+
+        Usuario.findOrCreate({
+                "login": profile.username
+            }, {
+                "nome": profile.username
+            },
             function (erro, usuario) {
-                
-                if(erro){
+
+                if (erro) {
                     console.log(erro);
                     return done(erro);
                 }
                 return done(null, usuario);
             }
         );
-        
+
     }));
-    
-    passport.serializeUser(function(usuario, done) {
+
+    passport.serializeUser(function (usuario, done) {
         done(null, usuario._id);
     });
-    
+
     passport.deserializeUser(function (id, done) {
-        
+
         Usuario.findById(id).exec().then(function (usuario) {
-           done(null, usuario); 
+            done(null, usuario);
         });
-        
+
     });
-    
+
 };
