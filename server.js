7f63eb979d57d64f8c63e492ce701243d081426b
Bonaldo <dnllink@hotmail.com>
2015-11-22 20:10:38 -0200

    projeto revertido ao ultimo commit estavel

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var http = require('http');
 var app = require('./config/express')(app);
-var config = require('./config/config')();
 
 require('./config/passport')();
-require('./config/database.js')(config.db);
+require('./config/database.js')('mongodb://localhost/contatooh');
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express Server escutando na porta ' + app.get('port'));
