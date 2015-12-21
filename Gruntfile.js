9e3013232c4d175ba41705a58e790c5fd9b84d26
Bonaldo <dnllink@hotmail.com>
2015-12-21 21:48:21 -0200

    commit final para deploy automatico

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,37 @@
 module.exports = function (grunt) {
-    
+
     grunt.initConfig({
+
+        usemin: {
+            html: 'app/views/**/*.ejs'
+        },
+
+        useminPrepare: {
+            options: {
+                root: 'public',
+                dest: 'public'
+            },
+            html: 'app/views/**/*.js'
+        },
+
+        ngAnnotate: {
+            scripts: {
+                expand: true,
+                src: ['public/js/**/*.js']
+            }
+        }
+
+    });
+
+    grunt.registerTask('minifica', ['useminPrepare', 'ngAnnotate', 'concat', 'uglify', 'cssmin', 'usemin']);
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-ng-annotate');
+
+    /*grunt.initConfig({
         copy: {
             project: {
                 expand: true,
@@ -15,33 +46,67 @@ module.exports = function (grunt) {
             }
         },
         usemin: {
-            html: 'dist/app/views/**/*.ejs'
-        },
-        useminPrepare: {
-            options: {
-                root: 'dist/public',
-                dest: 'dist/public'
+            html: 'dist/app/views/**/
+    /*.ejs'
             },
-            html: 'dist/app/views/**/*.ejs'
-        },
-        ngAnnotate: {
-            scripts: {
-                expand: true,
-                src: ['dist/public/js/**/*.js']
+            useminPrepare: {
+                options: {
+                    root: 'dist/public',
+                    dest: 'dist/public'
+                },
+                html: 'dist/app/views/**/
+    /*.ejs'
+            },
+            ngAnnotate: {
+                scripts: {
+                    expand: true,
+                    src: ['dist/public/js/**/
+    /*.js']
+                }
             }
-        }
-    });
-    
-    grunt.registerTask('default', ['dist', 'minifica']);
-    grunt.registerTask('dist', ['clean', 'copy']);
-    grunt.registerTask('minifica', ['useminPrepare', 'ngAnnotate', 'concat', 'uglify', 'cssmin', 'usemin']);
-    
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-usemin');
-    grunt.loadNpmTasks('grunt-ng-annotate');
-    
+        });
+
+        grunt.registerTask('default', ['dist', 'minifica']);
+        grunt.registerTask('dist', ['clean', 'copy']);
+        grunt.registerTask('minifica', ['useminPrepare', 'ngAnnotate', 'concat', 'uglify', 'cssmin', 'usemin']);
+
+        grunt.loadNpmTasks('grunt-contrib-copy');
+        grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-contrib-concat');
+        grunt.loadNpmTasks('grunt-contrib-uglify');
+        grunt.loadNpmTasks('grunt-contrib-cssmin');
+        grunt.loadNpmTasks('grunt-usemin');
+        grunt.loadNpmTasks('grunt-ng-annotate');*/
+
 };
+
+grunt.initConfig({
+
+    usemin: {
+        html: 'app/views/**/*.ejs'
+    },
+
+    useminPrepare: {
+        options: {
+            root: 'public',
+            dest: 'public'
+        },
+        html: 'app/views/**/*.js'
+    },
+
+    ngAnnotate: {
+        scripts: {
+            expand: true,
+            src: ['public/js/**/*.js']
+        }
+    }
+
+});
+
+grunt.registerTask('minifica', ['useminPrepare', 'ngAnnotate', 'concat', 'uglify', 'cssmin', 'usemin']);
+
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-usemin');
+grunt.loadNpmTasks('grunt-ng-annotate');
